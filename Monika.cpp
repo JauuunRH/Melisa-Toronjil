f34c8a0c0ad64bc05dfb2b46650d789431e73e2a
Jauuuun <126738246+JauuunRH@users.noreply.github.com>
2023-03-02 11:01:42 -0600

    Add files via upload

---
+++ Monika.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+int x;
+char r;
+  for (x=1;x<=15;x++){
+    cout << "¿Quieres ser mi tilina?";
+    cin >> r;
+     if (r == 's')
+{
+    cout << "viva Dios" << endl;
+    break;
+
+  }
+
+
+}
+  return 0;
+}
