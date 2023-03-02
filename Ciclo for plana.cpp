d2367a054861d743fc68523557bd65d7668b835d
Jauuuun <126738246+JauuunRH@users.noreply.github.com>
2023-03-02 10:43:55 -0600

    Add files via upload

---
+++ Ciclo for plana.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+int x;
+  for (x=1; x<=30; x++)
+  {
+           cout<<x<<"Viva Dios"<<endl;
+  }
+  return 0;
+}
---
+++ ciclo for.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+int x;
+  for (x = 100; x>=0; x-=5)
+  {
+           cout<<x<<" ";
+  }
+  return 0;
+}
+
+
