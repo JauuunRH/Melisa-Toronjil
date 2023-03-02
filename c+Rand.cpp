fef77a679ccd044958dd64c2b28365b3fdc8454f
Jauuuun <126738246+JauuunRH@users.noreply.github.com>
2023-03-02 11:20:13 -0600

    Add files via upload

---
+++ c+Rand.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  srand (time(NULL));
+int numAleatorio;
+
+numAleatorio;
+  numAleatorio=rand()%100;
+
+  cout<<"El numero aleatorio es: "<<numAleatorio;
+
+  return 0;
+}
