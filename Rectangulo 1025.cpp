faf3eb44944b326de67c418789df25b4ef6252ff
Jauuuun <126738246+JauuunRH@users.noreply.github.com>
2023-03-19 21:20:40 -0600

    Add files via upload

---
+++ Rectangulo 1025.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+  const int filas = 10;
+  const int columna = 25;
+
+for(int i = 0; i < filas; i++){
+for(int j = 0; j<columna; j++){
+cout<< "*";
+}
+cout<<endl;
+}
+ return 0;
+}
