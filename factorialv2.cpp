ba541ac231caca6472e8d873accca6d66a065410
Jauuuun <126738246+JauuunRH@users.noreply.github.com>
2023-03-19 21:36:17 -0600

    Add files via upload

---
+++ factorialv2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int num, factorial = 1;
+
+   //Pedir al usuario que ingrese un numero
+cout<<"Ingresa un numero entero 1 y 10:";
+cin>>num;
+
+   //Validar que el numero este en el rango permitido
+   if(num<1 || num>10){
+cout<<"El numero ingresado no esta dentro del rango permitido."<<endl;
+   return 0;
+}
+
+   //Calcular factorial del numero
+   for(int i=1; i<=num; i++){
+    factorial *=1;
+   }
+
+cout<<"El factorial de: "<<num<<" es: "<< factorial<< endl;
+
+   return 0;
+}
